67ae71f55673e13d02b1d1b4c9a0289d2e8eb96a
Sky Jacobson <sky.jacobson231@gmail.com>
2017-12-22 15:23:15 -0800

    mobile fix on header

--- src/App.js
+++ src/App.js
@@ -13,17 +13,19 @@ class App extends Component {
 				</div>
 				<header className="App-header">
 					<div className="Header-content">
-						<h1>Intelligent Insights</h1>
-						<h1>Data-driven Decisions</h1>
-						<p>
-							Helping leaders solve complex problems using advanced machine
-							learning to generate revenue, reduce costs and move audiences.
-						</p>
-						<div>
-							<a href="#Services">
-								Learn More
-								<span>&rarr;</span>
-							</a>
+						<div className="Header-middle">
+							<h1>Intelligent Insights</h1>
+							<h1>Data-driven Decisions</h1>
+							<p>
+								Helping leaders solve complex problems using advanced machine
+								learning to generate revenue, reduce costs and move audiences.
+							</p>
+							<div>
+								<a href="#Services">
+									Learn More
+									<span>&rarr;</span>
+								</a>
+							</div>
 						</div>
 					</div>
 				</header>
