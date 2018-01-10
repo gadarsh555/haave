4ea9a12859d329aa0591e20abe95f4bf95fab4b5
Sky Jacobson <sky.jacobson231@gmail.com>
2018-01-10 12:11:25 -0800

    nodemailer using less secure

--- server.js
+++ server.js
@@ -3,41 +3,57 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const keys = require('./config/keys');
+const nodemailer = require('nodemailer');
+// const xoauth2 = require('xoauth2');
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function(req, res) {
-	return res.send('pong');
-});
-
-// Email
-const nodemailer = require('nodemailer');
-const xoauth2 = require('xoauth2');
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-		type: 'OAuth2',
+let transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
 		user: 'contact.haave@gmail.com',
-		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret,
-		refreshToken: keys.googleRefreshToken,
-		accessToken: keys.googleAccessToken
-  }
+		pass: 'testPassword!'
+	}
 });
-
-var mailOptions = {
-  from: 'Sky <sky.jacobson231@gmail.com>',
-  to: 'Nora <contact.haave@gmail.com>',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+const mailOptions = {
+	from: 'contact.haave@gmail.com',
+	to: 'sky.jacobson231@gmail.com',
+	subject: 'Node contact request',
+	html: '<p>hello world</p>'
 };
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
+transporter.sendMail(mailOptions, (err, info) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(info);
+	}
 });
+// Email
+// app.post('/contact', (req, res) => {
+// let transporter = nodemailer.createTransport({
+// 	service: 'gmail',
+// 	auth: {
+// 		type: 'OAuth2',
+// 		user: 'contact.haave@gmail.com',
+// 		clientID: keys.googleClientID,
+// 		clientSecret: keys.googleClientSecret,
+// 		refreshToken: keys.googleRefreshToken,
+// 		accessToken: keys.googleAccessToken
+// 	}
+// });
+// let mailOptions = {
+// 	from: 'Client <contact.haave@gmail.com>',
+// 	to: 'Nora <contact.haave@gmail.com>',
+// 	subject: 'Sending Email using Node.js',
+// 	text: 'That was easy!'
+// };
+// transporter.sendMail(mailOptions, function(err, info) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log('Email sent: ' + info.response);
+// 	}
+// });
+// })
 // End Email
 
 app.get('/', function(req, res) {
