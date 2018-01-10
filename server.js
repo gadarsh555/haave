2598ce663619f4a56b7c380c6cd3cbf2d44f4269
Sky Jacobson <sky.jacobson231@gmail.com>
2018-01-10 15:40:46 -0800

    working email form

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
 const path = require('path');
 const app = express();
 const keys = require('./config/keys');
@@ -7,57 +8,61 @@ const nodemailer = require('nodemailer');
 // const xoauth2 = require('xoauth2');
 app.use(express.static(path.join(__dirname, 'build')));
 
-let transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: 'contact.haave@gmail.com',
-		pass: 'testPassword!'
-	}
-});
-const mailOptions = {
-	from: 'contact.haave@gmail.com',
-	to: 'sky.jacobson231@gmail.com',
-	subject: 'Node contact request',
-	html: '<p>hello world</p>'
-};
-transporter.sendMail(mailOptions, (err, info) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(info);
-	}
-});
-// Email
-// app.post('/contact', (req, res) => {
-// let transporter = nodemailer.createTransport({
-// 	service: 'gmail',
-// 	auth: {
-// 		type: 'OAuth2',
-// 		user: 'contact.haave@gmail.com',
-// 		clientID: keys.googleClientID,
-// 		clientSecret: keys.googleClientSecret,
-// 		refreshToken: keys.googleRefreshToken,
-// 		accessToken: keys.googleAccessToken
-// 	}
-// });
-// let mailOptions = {
-// 	from: 'Client <contact.haave@gmail.com>',
-// 	to: 'Nora <contact.haave@gmail.com>',
-// 	subject: 'Sending Email using Node.js',
-// 	text: 'That was easy!'
-// };
-// transporter.sendMail(mailOptions, function(err, info) {
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log('Email sent: ' + info.response);
-// 	}
-// });
-// })
-// End Email
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
+// Email
+app.post('/contact', (req, res) => {
+	console.log(req.body);
+	let transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'contact.haave@gmail.com',
+			pass: 'testPassword!'
+		}
+	});
+	const mailOptions = {
+		from: 'contact.haave@gmail.com',
+		to: 'sky.jacobson231@gmail.com',
+		subject: 'Contact Page',
+		html: `<p>Name: ${req.body.name}</p>
+					<p>Company: ${req.body.company}</p>
+					<p>Email: ${req.body.email}</p>
+					<p>Phone: ${req.body.phone}</p>
+					<p>Message: ${req.body.message}</p>`
+	};
+	transporter.sendMail(mailOptions, (err, info) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(info);
+		}
+	});
+});
+
+app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// Send all other requests to the React App
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'POST, GET, PATCH, DELETE, OPTIONS'
+	);
+	next();
+});
+
+//Set Port
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,58 @@ import React, { Component } from 'react';
 import '../styles/Contact.css';
 
 class Contact extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			company: '',
+			email: '',
+			phone: '',
+			message: ''
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(event) {
+		let change;
+		switch (event.target.placeholder) {
+			case 'Name':
+				change = { name: event.target.value };
+				break;
+			case 'Company':
+				change = { company: event.target.value };
+				break;
+			case 'Email':
+				change = { email: event.target.value };
+				break;
+			case 'Phone':
+				change = { phone: event.target.value };
+				break;
+			case 'Message':
+				change = { message: event.target.value };
+				break;
+			default:
+				change = {};
+		}
+		this.setState(change);
+	}
+	handleSubmit(event) {
+		fetch('http://localhost:8080/contact', {
+			method: 'POST',
+			headers: {
+				// Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name: this.state.name,
+				company: this.state.company,
+				email: this.state.email,
+				phone: this.state.phone,
+				message: this.state.message
+			})
+		});
+		event.preventDefault();
+	}
 	render() {
 		return (
 			<div id="contact-container">
@@ -10,12 +62,37 @@ class Contact extends Component {
 					<hr />
 				</div>
 				<div>
-					<form className="form">
-						<input className="form-control" placeholder="Name" />
-						<input className="form-control" placeholder="Company" />
-						<input className="form-control" placeholder="Email" />
-						<input className="form-control" placeholder="Phone" />
-						<textarea className="form-control" placeholder="Message" />
+					<form className="form" onSubmit={this.handleSubmit}>
+						<input
+							className="form-control"
+							placeholder="Name"
+							value={this.state.name}
+							onChange={this.handleChange}
+						/>
+						<input
+							className="form-control"
+							placeholder="Company"
+							value={this.state.company}
+							onChange={this.handleChange}
+						/>
+						<input
+							className="form-control"
+							placeholder="Email"
+							value={this.state.email}
+							onChange={this.handleChange}
+						/>
+						<input
+							className="form-control"
+							placeholder="Phone"
+							value={this.state.phone}
+							onChange={this.handleChange}
+						/>
+						<textarea
+							className="form-control"
+							placeholder="Message"
+							value={this.state.message}
+							onChange={this.handleChange}
+						/>
 						<button className="btn btn-primary" type="submit">
 							Submit
 						</button>
