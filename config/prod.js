12b691f238081fe94f68d9b78b33abe88354e09d
Sky Jacobson <sky.jacobson231@gmail.com>
2018-01-23 17:23:20 -0800

    include config:

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// prod.js - production keys go here
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  googleAccessToken: process.env.GOOGLE_ACCESS_TOKEN
+}
